528c68261f0afce5258c8f269e9d34c3e84eedcb
Matthew <matthew.minton@gmail.com>
2014-06-03 20:33:03 -0400

    modified js so that it actually counts down, not up

--- js/game.js
+++ js/game.js
@@ -1,5 +1,15 @@
 var countdown = prompt('How many seconds in the countdown?');
 countdown = parseInt(countdown);
 
-setTimeout(function(){console.log('You are taking off to the moon!');}, countdown);
+// setTimeout(function(){console.log('You are taking off to the moon!');}, countdown);
 
+function countItDown(){
+  if(countdown === 0){
+    clearInterval(countingDown);
+    console.log('You are taking off to the moon!');
+  } else {
+    console.log(countdown--);
+  }
+}
+
+var countingDown = setInterval(countItDown, 1000);
