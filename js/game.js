76188d0a7e48cdda55cac8afaeda169f4a7adc70
Matthew <matthew.minton@gmail.com>
2014-06-03 20:13:03 -0400

    functioning setTimeout functionality

--- js/game.js
+++ js/game.js
@@ -1,7 +1,5 @@
 var countdown = prompt('How many seconds in the countdown?');
 countdown = parseInt(countdown);
 
-
-
-
+setTimeout(function(){console.log('You are taking off to the moon!');}, countdown);
 
